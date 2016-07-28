beb7675fdf380917da1e04d625d49bd748046421
John Martens <jmartens@scdsb.on.ca>
2016-07-28 13:12:28 +0000

    prototype chart has been made

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var mongoose=require('mongoose'),
     express=require('express'),
     app=express();
     
-mongoose.connect("mongodb://localhost/bethere");
+mongoose.connect("mongodb://localhost/data");
+
+//QUANDL API KEY: wD2kaqRh4j7LCzf3bJJL
+//Sample call: https://www.quandl.com/api/v3/datasets/WIKI/FB.json?api_key=wD2kaqRh4j7LCzf3bJJL
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -13,8 +16,8 @@ app.get('/', function(req, res){
     res.render('index.ejs');
 });
 
-//to compile less at command line:
-// $ lessc styles.less styles.css
+//to compile less at command line for this project:
+// $ lessc bootstrap/bootstrap.less public/stylesheets/bootstrap.css
 //to minify js 
 //minify public/sylesheets/bootstrap.css public/stylesheets/bootstrap.min.css
 
